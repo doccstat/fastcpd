7740723557082488e09fcad8d371e9a559befb1e
Anthony Li <38049831+doccstat@users.noreply.github.com>
2023-04-17 23:14:21 -0500

    Add some tests to cost_update_hessian

--- src/cost_update_hessian.cpp
+++ src/cost_update_hessian.cpp
@@ -28,3 +28,15 @@ arma::mat cost_update_hessian(arma::mat data,
     }
     return hessian;
 }
+
+context("cost_update_hessian Unit Test") {
+  test_that("binomal is correct for a two dimensional data") {
+    arma::colvec theta = {-0.5, 0.3};
+    arma::mat data = {{1, 1, 0.2}};
+    std::string family = "binomial";
+    double min_prob = 0.0;
+    arma::mat hessian = cost_update_hessian(data, theta, family, min_prob);
+    arma::mat hessian_expected = {{0.238280, 0.0476560}, {0.047656, 0.0095312}};
+    expect_true(arma::norm(hessian - hessian_expected, "fro") < 0.000001);
+  }
+}
