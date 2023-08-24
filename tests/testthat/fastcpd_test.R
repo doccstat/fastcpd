0688b003b01905b0f2e42fdea54a175f7e09eac3
Anthony Li <38049831+doccstat@users.noreply.github.com>
2023-08-24 17:18:35 -0500

    Add confidence interval exploration tests

--- tests/testthat/fastcpd_test.R
+++ tests/testthat/fastcpd_test.R
@@ -102,3 +102,81 @@ test_that("penalized linear regression", {
 
   expect_equal(result@cp_set, c(300, 700, 1000))
 })
+
+test_that("confidence interval experiment", {
+  set.seed(1)
+  kDimension <- 1
+  change_point_locations <- NULL
+  for (experiment_id in seq_len(20)) {
+    data <- rbind(
+      mvtnorm::rmvnorm(300, mean = rep(0, kDimension), sigma = diag(100, kDimension)),
+      mvtnorm::rmvnorm(400, mean = rep(50, kDimension), sigma = diag(100, kDimension)),
+      mvtnorm::rmvnorm(300, mean = rep(2, kDimension), sigma = diag(100, kDimension))
+    )
+    segment_count_guess <- 10
+    block_size <- max(floor(sqrt(nrow(data)) / (segment_count_guess + 1)), 2)
+    block_count <- floor(nrow(data) / block_size)
+    data_all_vars <- rep(0, block_count)
+    for (block_index in seq_len(block_count)) {
+      block_start <- (block_index - 1) * block_size + 1
+      block_end <- if (block_index < block_count) block_index * block_size else nrow(data)
+      data_all_vars[block_index] <- var(data[block_start:block_end, ])
+    }
+    data_all_var <- mean(data_all_vars)
+    mean_loss <- function(data) {
+      n <- nrow(data)
+      (norm(data, type = "F")^2 - colSums(data)^2 / n) / 2 / data_all_var +
+        n / 2 * (log(data_all_var) + log(2 * pi))
+    }
+    mean_loss_result <- fastcpd(
+      formula = ~ . - 1,
+      data = data.frame(data),
+      beta = (kDimension + 1) * log(nrow(data)) / 2,
+      p = kDimension,
+      cost = mean_loss
+    )
+    change_point_locations <- c(change_point_locations, mean_loss_result@cp_set)
+  }
+
+  change_point_locations_cookie_bucket <- NULL
+  cookie_bucket_id_list <- sample.int(n = 20, size = 1000, replace = TRUE)
+  all_data <- data
+  for (cookie_bucket_id in seq_len(20)) {
+    data <- all_data[cookie_bucket_id_list != cookie_bucket_id, , drop = FALSE]
+    segment_count_guess <- 10
+    block_size <- max(floor(sqrt(nrow(data)) / (segment_count_guess + 1)), 2)
+    block_count <- floor(nrow(data) / block_size)
+    data_all_vars <- rep(0, block_count)
+    for (block_index in seq_len(block_count)) {
+      block_start <- (block_index - 1) * block_size + 1
+      block_end <- if (block_index < block_count) block_index * block_size else nrow(data)
+      data_all_vars[block_index] <- var(data[block_start:block_end, ])
+    }
+    data_all_var <- mean(data_all_vars)
+    mean_loss <- function(data) {
+      n <- nrow(data)
+      (norm(data, type = "F")^2 - colSums(data)^2 / n) / 2 / data_all_var +
+        n / 2 * (log(data_all_var) + log(2 * pi))
+    }
+    mean_loss_result <- fastcpd(
+      formula = ~ . - 1,
+      data = data.frame(data),
+      beta = (kDimension + 1) * log(nrow(data)) / 2,
+      p = kDimension,
+      cost = mean_loss
+    )
+
+    for (cp in mean_loss_result@cp_set) {
+      ordinal_mapped_cp <- which(cumsum(cookie_bucket_id_list != cookie_bucket_id) == cp)[1]
+      change_point_locations_cookie_bucket <- c(change_point_locations_cookie_bucket, ordinal_mapped_cp)
+    }
+  }
+  
+  table_change_point_locations <- table(change_point_locations)
+  expect_equal(rownames(table_change_point_locations), c("269", "299", "300", "700"))
+  expect_equal(unname(table_change_point_locations), c(1, 1, 19, 20), ignore_attr = TRUE)
+
+  table_change_point_locations_cookie_bucket <- table(change_point_locations_cookie_bucket)
+  expect_equal(rownames(table_change_point_locations_cookie_bucket), c("299", "300", "697", "700"))
+  expect_equal(unname(table_change_point_locations_cookie_bucket), c(1, 19, 1, 19), ignore_attr = TRUE)
+})
