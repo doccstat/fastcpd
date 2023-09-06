020e9cc4bfe8fe7ec9c20cf337e2cf6cdf89df57
Anthony Li <38049831+doccstat@users.noreply.github.com>
2023-09-06 18:01:24 +0000

    Fix one-dimensional linear regression example bug.

--- R/fastcpd.R
+++ R/fastcpd.R
@@ -82,7 +82,7 @@ NULL
 #' set.seed(1)
 #' p <- 1
 #' x <- mvtnorm::rmvnorm(300, rep(0, p), diag(p))
-#' theta_0 <- rbind(c(1, 1.2, -1), c(-1, 0, 0.5), c(0.5, -0.3, 0.2))
+#' theta_0 <- matrix(c(1, -1, 0.5))
 #' y <- c(
 #'   x[1:100, ] * theta_0[1, ] + rnorm(100, 0, 1),
 #'   x[101:200, ] * theta_0[2, ] + rnorm(100, 0, 1),
