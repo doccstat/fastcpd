8d2379350f2a4f6ba0356760972856c697d642c0
Anthony Li <38049831+doccstat@users.noreply.github.com>
2023-10-29 12:30:34 -0500

    Fix poisson example.

--- tests/testthat/examples/fastcpd_poisson.R
+++ tests/testthat/examples/fastcpd_poisson.R
@@ -12,12 +12,5 @@ y <- c(
   rpois(200, exp(x[1101:1300, ] %*% theta_0)),
   rpois(200, exp(x[1301:n, ] %*% (theta_0 + delta)))
 )
-result <- fastcpd(
-  formula = y ~ . - 1,
-  data = data.frame(y = y, x = x),
-  beta = (p + 1) * log(n) / 2,
-  k = function(x) 0,
-  family = "poisson",
-  epsilon = 1e-5
-)
+result <- fastcpd.poisson(cbind(y, x), epsilon = 1e-5)
 summary(result)
