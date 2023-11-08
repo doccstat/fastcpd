913d24aa2aa524b0e1f03137ae30cd4bbe94f49a
Anthony Li <38049831+doccstat@users.noreply.github.com>
2023-11-07 22:49:26 -0600

    Add transcriptome data

--- data-raw/transcriptome.R
+++ data-raw/transcriptome.R
@@ -1,4 +1,4 @@
-data("ACGH", package = "ecp")
+load("data-raw/transcriptome.RData")
 transcriptome <- data.frame(ACGH$data, row.names = seq_len(nrow(ACGH$data)))
 colnames(transcriptome) <- ACGH$individual
 usethis::use_data(transcriptome, overwrite = TRUE)
