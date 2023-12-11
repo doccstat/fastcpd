58878f133185251c067471326ec0eafc391d146d
Anthony Li <38049831+doccstat@users.noreply.github.com>
2023-12-11 14:16:25 -0600

    Update Poisson example

--- tests/testthat/examples/fastcpd_poisson.R
+++ tests/testthat/examples/fastcpd_poisson.R
@@ -1,5 +1,5 @@
 set.seed(1)
-n <- 1500
+n <- 1100
 p <- 3
 x <- mvtnorm::rmvnorm(n, rep(0, p), diag(p))
 delta <- rnorm(p)
@@ -8,9 +8,7 @@ y <- c(
   rpois(300, exp(x[1:300, ] %*% theta_0)),
   rpois(400, exp(x[301:700, ] %*% (theta_0 + delta))),
   rpois(300, exp(x[701:1000, ] %*% theta_0)),
-  rpois(100, exp(x[1001:1100, ] %*% (theta_0 - delta))),
-  rpois(200, exp(x[1101:1300, ] %*% theta_0)),
-  rpois(200, exp(x[1301:n, ] %*% (theta_0 + delta)))
+  rpois(100, exp(x[1001:n, ] %*% (theta_0 - delta)))
 )
 result <- suppressWarnings(
   fastcpd.poisson(cbind(y, x), epsilon = 1e-5, k = function(x) 1)
