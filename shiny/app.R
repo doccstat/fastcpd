d05f24598685eda3d6d5e8cb39b19e6f26fe3087
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-02-07 22:04:28 -0600

    Add family selection in R shiny app

--- shiny/app.R
+++ shiny/app.R
@@ -1,37 +1,90 @@
 library(shiny)
 
-# Define UI for app that draws a histogram
+# Define UI for the app
 ui <- fluidPage(
 
   # App title
   titlePanel("fastcpd - Fast Change Point Detection"),
 
-  # Sidebar layout with input and output definitions
+  selectInput(
+    "family",
+    "Choose a model family:",
+    list(
+      `Unlabeled Data` = list(
+        "Mean Change" = "mean",
+        "Variance Change" = "variance",
+        "Mean and/or Variance Change" = "meanvariance"
+      ),
+      `Time Series` = list(
+        "AR(p)" = "ar",
+        "MA(p)" = "ma",
+        "ARMA(p, q)" = "arma",
+        "ARIMA(p, d, q)" = "arima",
+        "GARCH(p, q)" = "garch",
+        "VAR(p)" = "var"
+      ),
+      `Regression Data` = list(
+        "Linear Regression" = "lm",
+        "Penalized Linear Regression" = "lasso",
+        "Logistic Regression" = "binomial",
+        "Poisson Regression" = "poisson"
+      ),
+      `DIY` = list(
+        "Custom" = "custom"
+      )
+    )
+  ),
+
+  # Sidebar layout for file upload
   sidebarLayout(
+    sidebarPanel(
+      fileInput(
+        inputId = "file",
+        label = "Select a csv file (with header)",
+        buttonLabel = "Select",
+        multiple = FALSE,
+        accept = c(".csv")
+      )
+    ),
 
-    # Sidebar panel for inputs
-    sidebarPanel(fileInput(
-      inputId = "upload",
-      label = "Select a csv file",
-      buttonLabel = "Select",
-      multiple = FALSE,
-      accept = c(".csv")
-    )),
-
-    # Main panel for displaying outputs
-    mainPanel(textOutput(outputId = "summary"))
+    mainPanel(verbatimTextOutput(outputId = "summary"))
   )
 )
 
 server <- function(input, output) {
-  output$summary <- renderText({
-    uploaded_file <- input$upload
-    if (is.null(uploaded_file)) {
-      return(NULL)
-    }
-    data <- read.csv(file = uploaded_file$datapath, header = TRUE, sep = ",")
-    fastcpd::fastcpd.lm(data)@cp_set
-  })
+
+  output$summary <- renderPrint(
+    {
+      input_file <- input$file
+      if (!is.null(input_file)) {
+        data <- read.csv(file = input_file$datapath)
+
+        # Use `fastcpd` wrapper functions so that a formula is not required.
+        fastcpd_wrapper <- switch(
+          input$family,
+          "mean" = fastcpd::fastcpd_mean,
+          "variance" = fastcpd::fastcpd_variance,
+          "meanvariance" = fastcpd::fastcpd_meanvariance,
+          "ar" = fastcpd::fastcpd_ar,
+          "ma" = fastcpd::fastcpd_ma,
+          "arma" = fastcpd::fastcpd_arma,
+          "arima" = fastcpd::fastcpd_arima,
+          "garch" = fastcpd::fastcpd_garch,
+          "var" = fastcpd::fastcpd_var,
+          "lm" = fastcpd::fastcpd_lm,
+          "lasso" = fastcpd::fastcpd_lasso,
+          "binomial" = fastcpd::fastcpd_binomial,
+          "poisson" = fastcpd::fastcpd_poisson,
+          "custom" = fastcpd::fastcpd
+        )
+        result <- fastcpd_wrapper(
+          data = data
+        )
+        summary(result)
+      }
+    },
+  )
+
 }
 
 shinyApp(ui = ui, server = server)
