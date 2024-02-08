714adc4ef41b987e7f157a9f5863e85b0790bcec
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-02-07 22:41:08 -0600

    Add formula and beta selection for custom family in R shiny

--- shiny/app.R
+++ shiny/app.R
@@ -4,7 +4,12 @@ library(shiny)
 ui <- fluidPage(
 
   # App title
-  titlePanel("fastcpd - Fast Change Point Detection"),
+  titlePanel(
+    paste0(
+      "fastcpd - Fast Change Point Detection ",
+      "(for demonstration purposes and reasonable data sizes only)"
+    )
+  ),
 
   selectInput(
     "family",
@@ -35,6 +40,32 @@ ui <- fluidPage(
     )
   ),
 
+  conditionalPanel(
+    condition = "input.family == 'custom'",
+    textInput(
+      inputId = "formula",
+      label = "Enter a formula (e.g., y ~ . - 1):",
+      value = "y ~ . - 1"
+    )
+  ),
+
+  conditionalPanel(
+    condition = "input.family == 'custom'",
+    textInput(
+      inputId = "custom_family",
+      label = "Enter a family (e.g., mean):",
+      value = "custom"
+    )
+  ),
+
+  textInput(
+    inputId = "beta",
+    label = paste0(
+      "Enter penalty selection criterion (BIC, MBIC or MDL) or a numeric value:"
+    ),
+    value = "MBIC"
+  ),
+
   # Sidebar layout for file upload
   sidebarLayout(
     sidebarPanel(
@@ -59,27 +90,46 @@ server <- function(input, output) {
       if (!is.null(input_file)) {
         data <- read.csv(file = input_file$datapath)
 
+        # Extract penalty selection criterion (beta) or a numeric value
+        beta <- ifelse(
+          input$beta %in% c("BIC", "MBIC", "MDL"),
+          input$beta,
+          suppressWarnings(as.numeric(input$beta))
+        )
+
         # Use `fastcpd` wrapper functions so that a formula is not required.
-        fastcpd_wrapper <- switch(
-          input$family,
-          "mean" = fastcpd::fastcpd_mean,
-          "variance" = fastcpd::fastcpd_variance,
-          "meanvariance" = fastcpd::fastcpd_meanvariance,
-          "ar" = fastcpd::fastcpd_ar,
-          "ma" = fastcpd::fastcpd_ma,
-          "arma" = fastcpd::fastcpd_arma,
-          "arima" = fastcpd::fastcpd_arima,
-          "garch" = fastcpd::fastcpd_garch,
-          "var" = fastcpd::fastcpd_var,
-          "lm" = fastcpd::fastcpd_lm,
-          "lasso" = fastcpd::fastcpd_lasso,
-          "binomial" = fastcpd::fastcpd_binomial,
-          "poisson" = fastcpd::fastcpd_poisson,
-          "custom" = fastcpd::fastcpd
-        )
-        result <- fastcpd_wrapper(
-          data = data
-        )
+        if (input$family == "custom") {
+          result <- fastcpd::fastcpd(
+            formula = eval(parse(text = input$formula)),
+            data = data,
+            beta = beta,
+            family = input$custom_family
+          )
+        } else {
+          fastcpd_wrapper <- switch(
+            input$family,
+            "mean" = fastcpd::fastcpd_mean,
+            "variance" = fastcpd::fastcpd_variance,
+            "meanvariance" = fastcpd::fastcpd_meanvariance,
+            "ar" = fastcpd::fastcpd_ar,
+            "ma" = fastcpd::fastcpd_ma,
+            "arma" = fastcpd::fastcpd_arma,
+            "arima" = fastcpd::fastcpd_arima,
+            "garch" = fastcpd::fastcpd_garch,
+            "var" = fastcpd::fastcpd_var,
+            "lm" = fastcpd::fastcpd_lm,
+            "lasso" = fastcpd::fastcpd_lasso,
+            "binomial" = fastcpd::fastcpd_binomial,
+            "poisson" = fastcpd::fastcpd_poisson
+          )
+
+          # Run the fastcpd wrapper function
+          result <- fastcpd_wrapper(
+            data = data,
+            beta = beta
+          )
+        }
+
         summary(result)
       }
     },
