0bed7f606d857d9a515bcbc8103a3b5b3f6727a8
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-02-11 17:05:50 -0600

    Prepare R shiny test release

---
+++ shiny/server.R
@@ -0,0 +1,67 @@
+library(shiny)
+
+server <- function(input, output) {
+
+  output$summary <- renderPrint(
+    {
+      input_file <- input$file
+      if (!is.null(input_file)) {
+        data <- read.csv(file = input_file$datapath)
+
+        # Extract penalty selection criterion (beta) or a numeric value
+        beta <- ifelse(
+          input$beta %in% c("BIC", "MBIC", "MDL"),
+          input$beta,
+          suppressWarnings(as.numeric(input$beta))
+        )
+
+        # Use `fastcpd` wrapper functions so that a formula is not required.
+        if (input$family == "custom") {
+          cost <- NULL
+          cost_gradient <- NULL
+          cost_hessian <- NULL
+          if (input$custom_family == "custom") {
+            cost <- eval(parse(text = input$cost))
+            cost_gradient <- eval(parse(text = input$cost_gradient))
+            cost_hessian <- eval(parse(text = input$cost_hessian))
+          }
+          result <- fastcpd::fastcpd(
+            formula = eval(parse(text = input$formula)),
+            data = data,
+            beta = beta,
+            family = input$custom_family,
+            cost = cost,
+            cost_gradient = cost_gradient,
+            cost_hessian = cost_hessian
+          )
+        } else {
+          fastcpd_wrapper <- switch(
+            input$family,
+            "mean" = fastcpd::fastcpd_mean,
+            "variance" = fastcpd::fastcpd_variance,
+            "meanvariance" = fastcpd::fastcpd_meanvariance,
+            "ar" = fastcpd::fastcpd_ar,
+            "ma" = fastcpd::fastcpd_ma,
+            "arma" = fastcpd::fastcpd_arma,
+            "arima" = fastcpd::fastcpd_arima,
+            "garch" = fastcpd::fastcpd_garch,
+            "var" = fastcpd::fastcpd_var,
+            "lm" = fastcpd::fastcpd_lm,
+            "lasso" = fastcpd::fastcpd_lasso,
+            "binomial" = fastcpd::fastcpd_binomial,
+            "poisson" = fastcpd::fastcpd_poisson
+          )
+
+          # Run the fastcpd wrapper function
+          result <- fastcpd_wrapper(
+            data = data,
+            beta = beta
+          )
+        }
+
+        summary(result)
+      }
+    },
+  )
+
+}
---
+++ shiny/ui.R
@@ -0,0 +1,148 @@
+library(shiny)
+
+ui <- fluidPage(
+
+  theme = bslib::bs_theme(bootswatch = "flatly"),
+
+  # App title
+  titlePanel(
+    paste(
+      "fastcpd - Fast Change Point Detection",
+      "(for demonstration purposes and reasonable data sizes only)"
+    )
+  ),
+
+  selectInput(
+    "family",
+    "Choose a model family:",
+    list(
+      `Unlabeled Data` = list(
+        "Mean Change" = "mean",
+        "Variance Change" = "variance",
+        "Mean and/or Variance Change" = "meanvariance"
+      ),
+      `Time Series` = list(
+        "AR(p)" = "ar",
+        "MA(p)" = "ma",
+        "ARMA(p, q)" = "arma",
+        "ARIMA(p, d, q)" = "arima",
+        "GARCH(p, q)" = "garch",
+        "VAR(p)" = "var"
+      ),
+      `Regression Data` = list(
+        "Linear Regression" = "lm",
+        "Penalized Linear Regression" = "lasso",
+        "Logistic Regression" = "binomial",
+        "Poisson Regression" = "poisson"
+      ),
+      `DIY` = list(
+        "Custom" = "custom"
+      )
+    )
+  ),
+
+  conditionalPanel(
+    condition = "input.family == 'custom'",
+    textInput(
+      inputId = "formula",
+      label = "Enter a formula (e.g., y ~ . - 1):",
+      value = "y ~ . - 1"
+    )
+  ),
+
+  conditionalPanel(
+    condition = "input.family == 'custom'",
+    textInput(
+      inputId = "custom_family",
+      label = paste(
+        "Enter a family (e.g., mean).",
+        "Please keep the 'custom' family",
+        "if cost functions will be provided below:"
+      ),
+      value = "custom"
+    )
+  ),
+
+  conditionalPanel(
+    condition = "input.family == 'custom'",
+    textAreaInput(
+      inputId = "cost",
+      label = paste(
+        "Enter the cost function",
+        "(e.g., function(data) wiht optional `theta` parameter):"
+      ),
+      value = r"[
+function(data, theta) {
+  x <- data[, -1]
+  y <- data[, 1]
+  u <- x %*% theta
+  nll <- -y * u + log(1 + exp(u))
+  nll[u > 10] <- -y[u > 10] * u[u > 10] + u[u > 10]
+  sum(nll)
+}]",
+      width = "600px",
+      height = "250px"
+    )
+  ),
+
+  conditionalPanel(
+    condition = "input.family == 'custom'",
+    textAreaInput(
+      inputId = "cost_gradient",
+      label = paste(
+        "Enter the cost gradient function",
+        "(e.g., function(data, theta)):"
+      ),
+      value = r"[
+function(data, theta) {
+  x <- data[nrow(data), -1]
+  y <- data[nrow(data), 1]
+  c(-(y - 1 / (1 + exp(-x %*% theta)))) * x
+}]",
+      width = "600px",
+      height = "250px"
+    )
+  ),
+
+  conditionalPanel(
+    condition = "input.family == 'custom'",
+    textAreaInput(
+      inputId = "cost_hessian",
+      label = paste(
+        "Enter the cost Hessian function",
+        "(e.g., function(data, theta)):"
+      ),
+      value = r"[
+function(data, theta) {
+  x <- data[nrow(data), -1]
+  prob <- 1 / (1 + exp(-x %*% theta))
+  (x %o% x) * c((1 - prob) * prob)
+}]",
+      width = "600px",
+      height = "250px"
+    )
+  ),
+
+  textInput(
+    inputId = "beta",
+    label = paste0(
+      "Enter penalty selection criterion (BIC, MBIC or MDL) or a numeric value:"
+    ),
+    value = "MBIC"
+  ),
+
+  # Sidebar layout for file upload
+  sidebarLayout(
+    sidebarPanel(
+      fileInput(
+        inputId = "file",
+        label = "Select a csv file (with header)",
+        buttonLabel = "Select",
+        multiple = FALSE,
+        accept = c(".csv")
+      )
+    ),
+
+    mainPanel(verbatimTextOutput(outputId = "summary"))
+  )
+)
