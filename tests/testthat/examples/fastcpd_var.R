deb0ae5bcc59d4604113738940f4832d125c2e7f
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-03-18 00:25:06 -0500

    Use `pruning` parameter in VAR example

--- tests/testthat/examples/fastcpd_var.R
+++ tests/testthat/examples/fastcpd_var.R
@@ -1,14 +1,14 @@
 set.seed(1)
-n <- 800
+n <- 300
 p <- 2
 theta_1 <- matrix(c(-0.3, 0.6, -0.5, 0.4, 0.2, 0.2, 0.2, -0.2), nrow = p)
 theta_2 <- matrix(c(0.3, -0.4, 0.1, -0.5, -0.5, -0.2, -0.5, 0.2), nrow = p)
 x <- matrix(0, n + 2, p)
-for (i in 1:500) {
+for (i in 1:200) {
   x[i + 2, ] <- theta_1 %*% c(x[i + 1, ], x[i, ]) + rnorm(p, 0, 1)
 }
-for (i in 501:n) {
+for (i in 201:n) {
   x[i + 2, ] <- theta_2 %*% c(x[i + 1, ], x[i, ]) + rnorm(p, 0, 1)
 }
-result <- fastcpd.var(x, 2, cost_adjustment = NULL)
+result <- fastcpd.var(x, 2, pruning = FALSE)
 summary(result)
--- tests/testthat/test-examples-fastcpd_var.R
+++ tests/testthat/test-examples-fastcpd_var.R
@@ -1,6 +1,6 @@
 testthat::test_that(
   "examples/fastcpd_var.R", {
     source("examples/fastcpd_var.R")
-    testthat::expect_equal(result@cp_set, 518)
+    testthat::expect_equal(result@cp_set, 204)
   }
 )
