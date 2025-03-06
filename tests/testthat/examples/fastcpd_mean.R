a58cb4c880888f9782496de88c6dce2f92cebb19
Anthony Li <38049831+doccstat@users.noreply.github.com>
2025-03-05 20:46:22 -0600

    Fix printing in examples.

--- tests/testthat/examples/fastcpd_mean.R
+++ tests/testthat/examples/fastcpd_mean.R
@@ -6,8 +6,9 @@ if (requireNamespace("mvtnorm", quietly = TRUE)) {
     mvtnorm::rmvnorm(4e+5, mean = rep(50, p), sigma = diag(100, p)),
     mvtnorm::rmvnorm(3e+5, mean = rep(2, p), sigma = diag(100, p))
   )
-  (result_time <- system.time(
+  result_time <- system.time(
     result <- fastcpd.mean(data, r.progress = FALSE, cp_only = TRUE)
-  ))
+  )
+  print(result_time)
   summary(result)
 }
